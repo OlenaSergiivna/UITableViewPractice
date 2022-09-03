dedd7f7d0bd039150815b70ac745d1f5f3d1bea3
Olena <elenasergeevvnna@gmail.com>
2022-09-03 21:37:36 +0300

    Added simple table view.

--- UITableViewPractice/ViewController.swift
+++ UITableViewPractice/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var simpleTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        simpleTableView.delegate = self
+        simpleTableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    
+}
